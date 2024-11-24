ef0c94ef8945ee46dcb20dcfbdcdaef921e9db99
Dinesh Babu Vaddineni <89186228+dineshbabuvaddineni@users.noreply.github.com>
2024-11-25 01:05:14 +0530

    Add files via upload

---
+++ day1_Basics/FirstJavaProgramme.java
@@ -0,0 +1,12 @@
+package day1_Basics;
+
+public class FirstJavaProgramme 
+{
+	public static void main(String args[])
+	{
+		System.out.println("Welcome to Java");
+		System.out.println(10+20);
+		System.out.println("10+20");
+	}
+
+}
---
+++ day1_Basics/SecondJavaProgramme.java
@@ -0,0 +1,14 @@
+package day1_Basics;
+
+public class SecondJavaProgramme {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello.....");   //For getting the line Syso ctrl+Space 
+		System.out.println("Hello.....");
+		System.out.println("Hello.....");
+		System.out.println("Hello.....");
+		System.out.println("Hello.....");
+	}
+
+}
