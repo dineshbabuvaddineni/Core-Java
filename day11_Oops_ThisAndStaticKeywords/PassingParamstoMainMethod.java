4b082cd2c87a343577bea415e1e2aaa8af9f0c42
Dinesh Babu Vaddineni <89186228+dineshbabuvaddineni@users.noreply.github.com>
2024-12-02 23:07:15 +0530

    Add files via upload

---
+++ day11_Oops_ThisAndStaticKeywords/PassingParamstoMainMethod.java
@@ -0,0 +1,18 @@
+package day11_Oops_ThisAndStaticKeywords;
+
+public class PassingParamstoMainMethod {
+
+	public static void main(String[] args) {
+		
+		System.out.println(args.length);
+		for(String value:args) {
+			System.out.println(value);
+		}
+		
+		//System.out.println("testing.....");
+		
+		
+
+	}
+
+}
